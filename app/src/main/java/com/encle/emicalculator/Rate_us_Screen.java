2f4eaec0470575a686057c012f643a83009a5618
meetk1803 <meetkakadiya1803@gmail.com>
2024-03-16 18:49:44 +0530

    16 march new updated ppf and cic

---
+++ app/src/main/java/com/encle/emicalculator/Rate_us_Screen.java
@@ -0,0 +1,16 @@
+package com.encle.emicalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.emicalculator.R;
+
+public class Rate_us_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_us_screen);
+    }
+}
