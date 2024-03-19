bc3180a30054faa899bcd34e228c219cb81fdf4a
meetk1803 <meetkakadiya1803@gmail.com>
2024-03-19 18:28:18 +0530

    16 march new updated ppf and cic

--- app/src/main/java/com/encle/emicalculator/Splash_Screen_Privacy.java
+++ app/src/main/java/com/encle/emicalculator/Splash_Screen_Privacy.java
@@ -5,23 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.example.emicalculator.R;
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
 public class Splash_Screen_Privacy extends AppCompatActivity {
     ExtendedFloatingActionButton BTN_accept;
+    CheckBox chk_privacy;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_privacy);
 
+        chk_privacy=findViewById(R.id.chk_privacy);
         BTN_accept =findViewById(R.id.BTN_accept);
+
 
         BTN_accept.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(Splash_Screen_Privacy.this,Splash_Screen.class));
+                if(chk_privacy.isChecked()){
+
+                    startActivity(new Intent(Splash_Screen_Privacy.this,Splash_Screen.class));
+                }else {
+                    Toast.makeText(Splash_Screen_Privacy.this, "Please accept terms & conditions", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
     }
